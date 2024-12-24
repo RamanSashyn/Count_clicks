febe78c3799490af208defb9bb53cf0abd0c35e2
Raman <149716575+RamanSashyn@users.noreply.github.com>
2024-12-24 15:44:13 +0100

    Исправление ошибок

--- main.py
+++ main.py
@@ -9,112 +9,104 @@ def is_shorten_link(url):
     return parsed_url.netloc == "vk.cc"
 
 
-def get_shorten_url(VK_API, long_url):
-    try:
-        url = "https://api.vk.com/method/utils.getShortLink"
-        params = {"access_token": VK_API, "v": "5.199", "url": long_url}
+def get_shorten_url(access_token, long_url):
+    url = "https://api.vk.com/method/utils.getShortLink"
+    params = {"access_token": access_token, "v": "5.199", "url": long_url}
 
-        response = requests.get(url, params=params)
-        response.raise_for_status()
+    response = requests.get(url, params=params)
+    response.raise_for_status()
 
-        response_data = response.json()
+    response_data = response.json()
 
-        if "error" in response_data:
-            error_message = response_data["error"].get(
-                "error_msg", "Неизвестная ошибка"
-            )
-            error_code = response_data["error"].get(
-                "error_code", "Неизвестный код ошибки"
-            )
-            raise ValueError(
-                f"Ошибка VK API: {error_message} (Код ошибки: {error_code})"
-            )
+    if "error" in response_data:
+        error_message = response_data["error"].get(
+            "error_msg", "Неизвестная ошибка"
+        )
+        error_code = response_data["error"].get(
+            "error_code", "Неизвестный код ошибки"
+        )
+        raise ValueError(
+            f"Ошибка VK API: {error_message} (Код ошибки: {error_code})"
+        )
 
-        return response_data["response"]["short_url"]
-
-    except ValueError as error:
-        raise error
-
-    except requests.exceptions.HTTPError as error:
-        raise error
-
-    except Exception as error:
-        raise Exception(f"Неожиданная ошибка: {error}")
+    return response_data["response"]["short_url"]
 
 
 def count_clicks(
-    VK_API, short_url, interval="forever", intervals_count=1, extended=0
+    access_token, short_url, interval="forever", intervals_count=1, extended=0
 ):
     parsed_url = urlparse(short_url)
     key = parsed_url.path.split("/")[-1]
 
-    try:
-        url = "https://api.vk.com/method/utils.getLinkStats"
-        params = {
-            "access_token": VK_API,
-            "v": "5.199",
-            "key": key,
-            "interval": interval,
-            "intervals_count": intervals_count,
-            "extended": extended,
-        }
+    url = "https://api.vk.com/method/utils.getLinkStats"
+    params = {
+        "access_token": access_token,
+        "v": "5.199",
+        "key": key,
+        "interval": interval,
+        "intervals_count": intervals_count,
+        "extended": extended,
+    }
 
-        response = requests.get(url, params=params)
-        response.raise_for_status()
+    response = requests.get(url, params=params)
+    response.raise_for_status()
 
-        response_data = response.json()
+    response_data = response.json()
 
-        if "error" in response_data:
-            error_message = response_data["error"].get(
-                "error_msg", "Неизвестная ошибка"
-            )
-            error_code = response_data["error"].get(
-                "error_code", "Неизвестный код ошибки"
-            )
-            raise ValueError(
-                f"Ошибка VK API: {error_message} (Код ошибки: {error_code})"
-            )
+    if "error" in response_data:
+        error_message = response_data["error"].get(
+            "error_msg", "Неизвестная ошибка"
+        )
+        error_code = response_data["error"].get(
+            "error_code", "Неизвестный код ошибки"
+        )
+        raise ValueError(
+            f"Ошибка VK API: {error_message} (Код ошибки: {error_code})"
+        )
 
-        stats = response_data["response"]["stats"]
+    stats = response_data["response"]["stats"]
 
-        if stats:
-            return [f"Переходы по ссылке: {stat['views']}" for stat in stats]
-        else:
-            return ["Нет данных по переходам."]
+    if stats:
+        return [stat['views'] for stat in stats]
+    else:
+        return []
 
-    except ValueError as error:
-        raise error
 
-    except requests.exceptions.HTTPError as error:
-        raise error
-
-    except Exception as error:
-        raise Exception(f"Неожиданная ошибка: {error}")
+def print_clicks_info(clicks):
+    if clicks:
+        for click in clicks:
+            print(f"Переходы по ссылке: {click}")
+    else:
+        print("Нет данных по переходам.")
 
 
 def main():
     load_dotenv()
-    VK_API = os.getenv("VK_API")
+
+    try:
+        vk_access_token = os.environ["VK_ACCESS_TOKEN"]
+    except KeyError:
+        print("Переменная окружения VK_ACCESS_TOKEN не установлена.")
+        return
 
     long_url = input("Введите ссылку: ")
 
-    if is_shorten_link(long_url):
-        try:
-            clicks_info = count_clicks(VK_API, long_url)
-
-            for click in clicks_info:
-                print(click)
-
-        except Exception as error:
-            print(f"Ошибка при получении статистики: {error}")
-
-    else:
-        try:
-            short_url = get_shorten_url(VK_API, long_url)
+    try:
+        if is_shorten_link(long_url):
+            clicks_info = count_clicks(vk_access_token, long_url)
+            print_clicks_info(clicks_info)
+        else:
+            short_url = get_shorten_url(vk_access_token, long_url)
             print("Сокращенная ссылка:", short_url)
 
-        except Exception as error:
-            print(f"Ошибка при получении сокращенной ссылки: {error}")
+    except ValueError as error:
+        print(f"Ошибка: {error}")
+
+    except requests.exceptions.HTTPError as error:
+        print(f"Ошибка HTTP: {error}")
+
+    except KeyError as error:
+        print(f"Ошибка с ключом: {error}")
 
 
 if __name__ == "__main__":
